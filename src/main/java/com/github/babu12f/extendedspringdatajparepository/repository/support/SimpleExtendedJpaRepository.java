335462ccbdc58cedc7ba6dfc9c01102a2e6985b4
babor <babu_12f@yahoo.com>
2023-09-11 22:17:48 +0600

    Initial Commit

---
+++ src/main/java/com/github/babu12f/extendedspringdatajparepository/repository/support/SimpleExtendedJpaRepository.java
@@ -0,0 +1,27 @@
+package com.github.babu12f.extendedspringdatajparepository.repository.support;
+
+import com.github.babu12f.extendedspringdatajparepository.repository.ExtendedJpaRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class SimpleExtendedJpaRepository<T, ID extends Serializable> extends SimpleJpaRepository<T, ID> implements ExtendedJpaRepository<T, ID> {
+
+    private final JpaEntityInformation<T, ?> entityInformation;
+    private final EntityManager em;
+
+    public SimpleExtendedJpaRepository(JpaEntityInformation<T, ?> entityInformation, EntityManager entityManager) {
+        super(entityInformation, entityManager);
+        this.entityInformation = entityInformation;
+        this.em = entityManager;
+    }
+
+    @Override
+    @Deprecated
+    public List<T> test() {
+        return findAll();
+    }
+}
