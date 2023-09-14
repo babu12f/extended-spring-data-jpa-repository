76ee495a3079bc6de105383912628fc209fc051e
babor <babu_12f@yahoo.com>
2023-09-14 23:49:41 +0600

    Real all data by status check

--- src/main/java/com/github/babu12f/extendedspringdatajparepository/repository/ExtendedJpaRepository.java
+++ src/main/java/com/github/babu12f/extendedspringdatajparepository/repository/ExtendedJpaRepository.java
@@ -1,7 +1,9 @@
 package com.github.babu12f.extendedspringdatajparepository.repository;
 
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
@@ -9,4 +11,8 @@ import java.util.List;
 @NoRepositoryBean
 public interface ExtendedJpaRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
     List<T> test();
+
+    List<T> readAllDomainData();
+
+    List<T> readAllDomainData(@Nullable Specification<T> spec);
 }
